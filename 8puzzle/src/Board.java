4159cfade377a8bb0ef17e57553db002ede4fa9f
Steve Bastians <stevejb71@gmail.com>
2019-01-19 22:07:25 +0800

    8puzzle skeleton

---
+++ 8puzzle/src/Board.java
@@ -0,0 +1,45 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+package src;
+
+public class Board {
+    public Board(int[][] blocks) {
+
+    }
+
+    public int dimension() {
+        return 0;
+    }
+
+    public int hamming() {
+        return 0;
+    }
+
+    public int manhattan() {
+        return 0;
+    }
+
+    public boolean isGoal() {
+        return false;
+    }
+
+    public Board twin()   {
+        return null;
+    }
+
+    public boolean equals(Object y) {
+        return false;
+    }
+
+    public Iterable<Board> neighbors() {
+        return null;
+    }
+
+    public String toString() {
+        return "";
+    }
+}
---
+++ 8puzzle/src/Solver.java
@@ -0,0 +1,25 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+package src;
+
+public class Solver {
+    public Solver(Board initial) {
+
+    }
+
+    public boolean isSolvable() {
+        return false;
+    }
+
+    public int moves() {
+        return 0;
+    }
+
+    public Iterable<Board> solution() {
+        return null;
+    }
+}
