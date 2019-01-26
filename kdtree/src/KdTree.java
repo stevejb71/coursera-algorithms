934f271880e9ef30ff35e4ca45c821e7cb2ec0c5
Steve Bastians <stevejb71@gmail.com>
2019-01-26 11:57:08 +0800

    kdtree project start

---
+++ kdtree/src/KdTree.java
@@ -0,0 +1,20 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+public class KdTree {
+    public void insert(Point2D p) {
+
+    }
+
+    public void draw() {
+
+    }
+
+    public KdTree nearest(Point2D query) {
+        return null;
+    }
+
+    public Iterable<Point2D> range(RectHV rect) {
+        return null;
+    }
+}
